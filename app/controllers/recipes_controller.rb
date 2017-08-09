68e3246ab7c4a79d6117fb607c11c5a882b34ddf
Ollie <oliverbucks@gmail.com>
2017-08-09 21:07:29 +0000

    add recipe creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,34 @@
 class RecipesController < ApplicationController
   
   def index
-     @recipes = Recipe.all
+    @recipes = Recipe.all
   end
   
-
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = User.first
+    if @recipe.save
+      flash[:success] = "Recipe was created successfuly"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+  
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+ 
+  
 end
