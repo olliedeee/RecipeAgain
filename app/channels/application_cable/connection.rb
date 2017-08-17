0b4ab7c0b2691cb4308bc42d62bb4d8126a42e77
Ollie <oliverbucks@gmail.com>
2017-08-17 19:34:05 +0000

    connection.rb change

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,18 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     
-    identified_by :current_chef
+    identified_by :current_user
     def connect
-      self.current_chef = find_current_user
+      self.current_user = find_current_chef
     end
     
     def disconnect
     end
     
     protected
-    def find_current_user
-      if current_chef = User.find_by(id: cookies.signed[:user_id])
-      current_chef
+    def find_current_chef
+      if current_user = User.find_by(id: cookies.signed[:user_id])
+      current_user
       else
       reject_unauthorized_connection
       end
